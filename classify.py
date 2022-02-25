a1b2d02a5e582cf319431c1775ffbdbea9be5ab9
Kazimi <kazimib@win.desy.de>
2022-02-26 00:11:49 +0100

    main func

--- classify.py
+++ classify.py
@@ -3,13 +3,6 @@ import helper
 from jina import Flow
 
 
-# Layout
-st.set_page_config(page_title="Jina Pet Breed Classification")
-st.markdown(
-    body=helper.UI.css,
-    unsafe_allow_html=True,
-)
-
 # function to show image and its predicted breed
 def show_pet_and_breed(tags, image):
     """
@@ -22,23 +15,34 @@ def show_pet_and_breed(tags, image):
     article = 'an' if breed[0] in ['a', 'e', 'i', 'o', 'u'] else 'a' # the definite article for category to be printed!
     st.image(image, caption="I am {} percent sure this is {} {} {}".format(round(tags['prob']*100), article, breed , pet_category))
 
-# Sidebar
-st.sidebar.markdown(helper.UI.about_block, unsafe_allow_html=True)
 
-st.header("Jina Pet Breed Classification")
+def main():
+    # Layout
+    st.set_page_config(page_title="Jina Pet Breed Classification")
+    st.markdown(
+        body=helper.UI.css,
+        unsafe_allow_html=True,
+    )
 
-upload_cell, preview_cell = st.columns([12, 1])
-query = upload_cell.file_uploader("")
-if query:
-    doc, image = helper.convert_file_to_document(query)
-    if st.button(label="Classify"):
-        if not query:
-            st.markdown("Please upload a pet image")
-        else:
-            # Flow
-            flow = Flow().add(uses='jinahub://PetBreedClassifier')
-            with flow:
-                flow.post(on='/index', inputs=doc, on_done=lambda resp: show_pet_and_breed(resp.docs[0].tags, image))
+    # Sidebar
+    st.sidebar.markdown(helper.UI.about_block, unsafe_allow_html=True)
+
+    st.header("Jina Pet Breed Classification")
+
+    upload_cell, preview_cell = st.columns([12, 1])
+    query = upload_cell.file_uploader("")
+    if query:
+        doc, image = helper.convert_file_to_document(query)
+        if st.button(label="Classify"):
+            if not query:
+                st.markdown("Please upload a pet image")
+            else:
+                # Flow
+                flow = Flow().add(uses='jinahub://PetBreedClassifier')
+                with flow:
+                    flow.post(on='/index', inputs=doc, on_done=lambda resp: show_pet_and_breed(resp.docs[0].tags, image))
         
+if __name__ == '__main__':
+    main()
 
 
