b3f8f5a4028fc14f3133d8e1d8b98f8199c6b1a4
Kazimi <kazimib@win.desy.de>
2022-02-25 23:14:50 +0100

    jina pet breed classification first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Jina Pet breed classification using Jina
+
+This is a pet breed classification front end. The backend is built as [Jina Executor](https://github.com/Bashirkazimi/executor-pet-breed-classifier). The model is trained using [this](https://github.com/Bashirkazimi/pet-breed-classification) code and the [Oxford pets and dogs dataset](https://www.kaggle.com/zippyz/cats-and-dogs-breeds-classification-oxford-dataset).
+
+
+1. Clone this repo
+2. Enter the directory
+3. Create and activate virtualenv/conda env
+4. `pip install -r requirements.txt`
+5. `streamlit run classify.py`
+6. Open the URL in your browser
---
+++ classify.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import helper
+from jina import Flow
+
+
+# Flow
+flow = Flow().add(uses='jinahub://PetBreedClassifier')
+
+# Layout
+st.set_page_config(page_title="Jina Pet Breed Classification")
+st.markdown(
+    body=helper.UI.css,
+    unsafe_allow_html=True,
+)
+
+# function to show image and its predicted breed
+def show_pet_and_breed(tags, image):
+    """
+    Shows an image of a pet and prints out its predicted breed and probability using the tags dictionary
+    """
+    breed = tags['label'] # the predicted breed
+    pet_category = 'cat' if breed[0].isupper() else 'dog' # capitalized breed categories are cats, otherwise dogs
+    breed = breed.lower()
+    breed = ' '.join(breed.split('_')) # multi-word categories are joined using '_', so replace it with space
+    article = 'an' if breed[0] in ['a', 'e', 'i', 'o', 'u'] else 'a' # the definite article for category to be printed!
+    st.image(image, caption="I am {} percent sure this is {} {} {}".format(round(tags['prob']*100), article, breed , pet_category))
+
+# Sidebar
+st.sidebar.markdown(helper.UI.about_block, unsafe_allow_html=True)
+
+st.header("Jina Pet Breed Classification")
+
+upload_cell, preview_cell = st.columns([12, 1])
+query = upload_cell.file_uploader("")
+if query:
+    doc, image = helper.convert_file_to_document(query)
+    if st.button(label="Classify"):
+        if not query:
+            st.markdown("Please upload a pet image")
+        else:
+            with flow:
+                flow.post(on='/index', inputs=doc, on_done=lambda resp: show_pet_and_breed(resp.docs[0].tags, image))
+        
+
+
